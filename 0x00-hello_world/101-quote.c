7bbd82208e616d7b511519d29f890e8f92bc048a
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-16 23:42:51 +0100

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 int len;
 len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
 return (1);
 }
