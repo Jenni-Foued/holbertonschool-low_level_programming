f51330729e6bac9a6a78ca94efbc1581b25fbecc
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-16 23:55:53 +0100

    update 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,6 @@ printf("Size of a char:%i byte(s)\n", sizeof(char));
 printf("Size of an int:%i byte(s)\n", sizeof(int));
 printf("Size of a long int:%i byte(s)\n", sizeof(long int));
 printf("Size of a long long int:%i byte(s)\n", sizeof(long long int));
+printf("Size of a float:%i byte(s)\n", sizeof(float));
 return (0);
 }
