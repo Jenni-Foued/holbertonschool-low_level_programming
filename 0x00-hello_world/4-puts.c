0db50d6d2dbe72c298bdbd666eec38070c16652a
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-16 13:16:22 +0100

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main (void)
-{ puts("\"Programming is like building a multilingual puzzle");
-  return (0) ;
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
