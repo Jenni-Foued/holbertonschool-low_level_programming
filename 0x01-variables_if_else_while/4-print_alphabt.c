48db3e31507e2f0c9a2f2baa8f707e067f2c4ab8
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-17 02:50:26 +0100

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**                                                                             
+ * main -  Prints all the alphabets in lowercase except "q" and "e".                               
+ *                                                                              
+ * Return: Always 0 (Success)                                                   
+ */
+int main(void)
+{
+  int i ;
+  for (i=97;i<123;i++)
+    {
+      if (i != 113 && i != 101)
+	{
+      putchar(i);
+	}
+    }
+  putchar ('\n');
+
+    return (0);
+}
