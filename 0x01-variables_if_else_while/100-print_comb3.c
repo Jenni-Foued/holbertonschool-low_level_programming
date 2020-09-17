3faa154bfe12300b6ca1c9d4284a328ff81b8c55
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-17 22:18:09 +0100

    advanced task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ *main - print comb3.
+ *
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+int i;
+int j;
+int x;
+
+x = 48;
+for (i = 48; i < 58; i++)
+{
+for (j = x; j < 58; j++)
+{
+if (i != j)
+{
+putchar(i);
+putchar(j);
+if (!(i == 57 && j == 57))
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+x = x + 1;
+}
+putchar('\n');
+return (0);
+}
