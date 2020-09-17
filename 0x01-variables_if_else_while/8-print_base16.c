43f39b7d45f065fd40864df1ee639a5ec4f2de25
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-17 10:02:36 +0100

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (i = 97; i < 103; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
