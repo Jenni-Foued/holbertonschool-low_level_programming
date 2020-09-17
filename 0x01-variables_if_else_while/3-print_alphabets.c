f1c44f2261a6c538a1013fc2578c9f27666a131f
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-17 13:29:04 +0100

    update task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
-/**                                                                             
- * main -  Prints all the alphabets in lowercase then in upercase.                                    
- *                                                                              
- * Return: Always 0 (Success)                                                   
+/**
+ * main -  Prints all the alphabets in lowercase then in upercase.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int i ;
-  for (i=97;i<123;i++)
-    {
-      putchar(i);
-    }
-  for (i=65;i<91;i++)
-    {
-      putchar(i);
-    }
-  putchar ('\n');
-
-    return (0);
+int i;
+for (i = 97; i < 123; i++)
+{
+putchar(i);
+}
+for (i = 65; i < 91; i++)
+{
+putchar(i);
+}
+putchar ('\n');
+return (0);
 }
