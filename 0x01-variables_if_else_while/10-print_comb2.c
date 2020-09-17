c2f5731a94d0db1ad47e3d3b75dc908064c0b27c
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-17 21:43:36 +0100

    task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**                                                                       
+ *main - print the numbers from 00 to 99.
+ *
+ *Return: 0 (success)                                                          
+ */
+
+int main(void)
+{
+int i;
+int j;
+for (i = 48; i < 58; i++)
+{
+for (j = 48; j < 58; j++)
+{
+putchar(i);
+putchar(j);
+if (j < 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
+}
