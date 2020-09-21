a5c0a502ad9a2e2a41133ff6b181800a2c3b0ffe
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-21 13:43:18 +0100

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_sign: define if a number is positive or negative or is a zero
+ * n: the number to be tested
+ * Return: - return 1 if n>0 return 0 if n=0 return -1 if n<0
+ */
+int  print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+	return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+	_putchar('-');
+	return (-1);
+}
+}
