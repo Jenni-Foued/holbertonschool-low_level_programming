c94bbd1fcc473031d9053d6eb50aac917d9babb2
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-21 11:14:44 +0100

    first task

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char T[10] = "Holberton\n";
+int i = 0;
+
+for (i = 0; i < 10; i++)
+{
+_putchar(T[i]);
+}
+return (0);
+}
