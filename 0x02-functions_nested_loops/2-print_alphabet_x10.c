b710b3a83c059f29258027f90e0ecd74fe81faf6
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-21 12:38:24 +0100

    task 2 and 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - print alphabet [a..z]
+ *
+ * Return: - always (0)
+ */
+void print_alphabet_x10(void)
+{
+char i;
+int j;
+
+for (j = 0; j < 10; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
