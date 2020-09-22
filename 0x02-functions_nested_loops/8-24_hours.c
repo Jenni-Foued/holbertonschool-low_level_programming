97aad2131694556dd604815956abb73ca9f5f288
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-22 09:07:39 +0100

    update task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,27 +3,22 @@
 /**
  * jack_bauer - define is a character is alphabetic
  *
- * Return: - 1 if c is an alphabet 0 if not
+ * Return: - (void)
  */
 void jack_bauer(void)
 {
-int h1, h2, m1, m2;
+int h, m;
 
-for (h1 = 0; h1 <= 2; h1++)
+for (h = 0; h <= 23; h++)
 {
-for (h2 = 0; h2 <= 4; h2++)
+for (m = 0; m <= 59; m++)
 {
-for (m1 = 0; m1 <= 5; m1++)
-{
-for (m2 = 0; m2 <= 9; m2++)
-{
-_putchar(h1);
-_putchar(h2);
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
 _putchar(':');
-_putchar(m1);
-_putchar(m2);
-}
-}
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
 }
 }
 }
