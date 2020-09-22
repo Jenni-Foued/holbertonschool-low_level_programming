626b3bf4ab18df67a5543c53d889e99df8595eb0
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-22 08:44:15 +0100

    update task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,13 +7,19 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-{_putchar ((((n * -1) % 10) + '0'));
+if (n < 0)
+{
+_putchar ((((n * -1) % 10) + '0'));
 return (((n * -1)) % 10);
 }
-else
+else if (n > 0)
 {
 _putchar ((n % 10) + '0');
 return (n % 10);
+}
+else
+{
+_putchar (n + '0');
+return (0);
 }
 }
