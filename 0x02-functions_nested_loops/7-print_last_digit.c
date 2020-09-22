cf11df93ba3433553bd57cb59519d94c32799b7c
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-22 08:37:49 +0100

    update task 5

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @n: number to use.
+ * Return: the last digit of a number .
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+{_putchar ((((n * -1) % 10) + '0'));
+return (((n * -1)) % 10);
+}
+else
+{
+_putchar ((n % 10) + '0');
+return (n % 10);
+}
+}
