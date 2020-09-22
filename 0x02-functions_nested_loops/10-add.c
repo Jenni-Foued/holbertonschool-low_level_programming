70b41900b171c7ac6515ca8315d800b79eabe525
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-22 12:09:21 +0100

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+	* add - add 2 numbers.
+	* @i: first number.
+	* @j: second number.
+	* Return: the addition of two numbers.
+	*/
+
+int add(int i, int j)
+{
+return ((i + j));
+}
