76657e1d7a3ffd5ab278f73b896857d67538692a
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-23 11:57:09 +0100

    update task 2 advanced

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,8 +8,8 @@
 int main(void)
 {
 int i, n;
-n = 0;
 
+n = 0;
 for (i = 3; i < 1024; i++)
 {
 if (((i % 3) == 0) || ((i % 5) == 0))
@@ -18,4 +18,5 @@ n = n + i;
 }
 }
 printf("%d\n", n);
+return (0);
 }
