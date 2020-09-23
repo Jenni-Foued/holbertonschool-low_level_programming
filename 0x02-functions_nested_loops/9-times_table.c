4b7e2d0cdfd3b686b0d227dfbc3dd41a2d70c006
Med Foued Jenni <medfouedjenni@gmail.com>
2020-09-23 10:44:37 +0100

    update task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,36 +3,41 @@
 /**
 	* times_table - print the 9 times table, starting with 0.
  *
-	*
 	* Return: (void) .
 **/
 void times_table(void)
 {
-int i, j;
+int i, j, n;
+
 
 for (i = 0; i < 10; i++)
 {
-	for (j = 0; j < 10; j++)
-	{
-		if (j * i > 10)
-		{
-			_putchar(' ');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar(' ');
-		}
-		if ((i * j) < 10)
-		{
-			_putchar((i * j) + '0');
-		}
-		else
-		{
-			_putchar(((i * j) / 10) + '0');
-			_putchar(((i * j) % 10) + '0');
-		}
-	}
+_putchar(0 + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+for (j = 1; j < 10; j++)
+{
+n = i * j;
+if (n > 9)
+{
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
+else
+{
+_putchar(n + '0');
+}
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+if ((i * (j + 1)) < 10)
+{
+_putchar(' ');
+}
+}
+}
 _putchar('\n');
 }
 }
