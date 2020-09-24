8af427fd6a1e4a7afed9076994be0968eba35cf1
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-24 23:31:22 +0100

    update task 2 advanced

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,17 +9,15 @@
 
 void print_number(int n)
 {
-int s;
 
-s = 1;
 if (n < 0)
 {
-s = -1;
+_putchar('-');
+n = n * -1;
 }
-n = n *s;
 if (n >= 10000)
 {
-_putchar(((n / 10000) * s) +'0');
+_putchar((n / 10000) +'0');
 _putchar(((n % 10000) / 1000) + '0');
 _putchar(((n % 1000) / 100) + '0');
 _putchar(((n % 100) / 10) + '0');
@@ -27,20 +25,20 @@ _putchar((n % 10) + '0');
 }
 else if (n < 10000 && n >= 1000)
 {
-_putchar((((n % 10000) / 1000) * s) +'0');
+_putchar(((n % 10000) / 1000) +'0');
 _putchar(((n % 1000) / 100) + '0');
 _putchar(((n % 100) / 10) + '0');
 _putchar((n % 10) + '0');
 }
 else if (n < 1000 && n >= 100)
 {
-_putchar((((n % 1000) / 100) * s) +'0');
+_putchar(((n % 1000) / 100) +'0');
 _putchar(((n % 100) / 10) + '0');
 _putchar((n % 10) + '0');
 }
 else if (n < 100 && n >= 10)
 {
-_putchar((((n % 100) / 10) * s) +'0');
+_putchar(((n % 100) / 10) +'0');
 _putchar((n % 10) + '0');
 }
 else
