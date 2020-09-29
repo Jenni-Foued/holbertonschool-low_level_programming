39810638029f29ff88d6548f2226fef17398d1a2
salahbesebs <salah_besbes@ymail.com>
2020-09-29 15:01:09 +0100

    task 1 advanced betty not happy

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,39 @@
 #include "holberton.h"
 
 /**
- * _atoi -
- *@s:
- *Return:
+ * _atoi - convert a string into an integer.
+ *@s: the string to use
+ *Return: the converted integer.
  */
 
 int _atoi(char *s)
 {
+int neg = 0, i = 0, res = 0;
 
+while (!(s[i] <= '9' && s[i] >= '0' && s[i] != '\0'))
+{
+if (s[i] == '-')
+{
+neg++;
+}
+i++;
+}
+
+if (s[i] != '\0')
+{
+res = s[i] - 48;
+i++;
+}
+
+while (s[i] <= '9' && s[i] >= '0' && s[i] != '\0')
+{
+res = (res * 10) + (s[i] - 48);
+i++;
+}
+
+if (neg % 2 == 1)
+{
+res = res * -1;
+}
+return (res);
 }
