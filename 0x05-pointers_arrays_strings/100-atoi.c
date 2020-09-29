2c2f89366d39687df2369cd1e46a422707d3fa8a
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-29 18:45:42 +0100

    update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,21 +12,17 @@ int _atoi(char *s)
 {
 	int sign = 1, i = 0, res = 0;
 
-	while (!(s[i] <= '9' && s[i] >= '0' && s[i] != '\0'))
+	while (!(s[i] <= '9' && (s[i] >= '0' && s[i] != '\0')))
 	{
 		if (s[i] == '-')
-	{
 			sign *= -1;
-	}
 		i++;
 	}
-
-	while (s[i] <= '9' && s[i] >= '0' && s[i] != '\0')
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 	{
 		res = (res * 10) + (s[i] - 48);
 		i++;
 	}
-
 	res = res * sign;
 	return (res);
 }
