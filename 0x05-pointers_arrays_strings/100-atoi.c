a6dca1f86ea4d279327977769e8217a39d3c54a9
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-29 18:59:49 +0100

    last try 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,9 +3,9 @@
 /**
  * _atoi - convert a string into an integer.
  *
- *@s: the string to use
+ * @s: the string to use.
  *
- *Return: the converted integer.
+ * Return: integer.
  */
 
 int _atoi(char *s)
