299a98acf8c6ce35e78b66d4e143f2b3b6c97aad
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-29 17:56:41 +0100

    update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,32 +8,32 @@
 
 int _atoi(char *s)
 {
-int neg = 0, i = 0, res = 0;
+	int neg = 0, i = 0, res = 0;
 
-while (!(s[i] <= '9' && s[i] >= '0' && s[i] != '\0'))
-{
-if (s[i] == '-')
-{
-neg++;
-}
-i++;
-}
+	while (!(s[i] <= '9' && s[i] >= '0' && s[i] != '\0'))
+	{
+		if (s[i] == '-')
+	{
+			neg++;
+	}
+		i++;
+	}
 
-if (s[i] != '\0')
-{
-res = s[i] - 48;
-i++;
-}
+	if (s[i] != '\0')
+	{
+		res = s[i] - 48;
+		i++;
+	}
 
-while (s[i] <= '9' && s[i] >= '0' && s[i] != '\0')
-{
-res = (res * 10) + (s[i] - 48);
-i++;
-}
+	while (s[i] <= '9' && s[i] >= '0' && s[i] != '\0')
+	{
+		res = (res * 10) + (s[i] - 48);
+		i++;
+	}
 
-if (neg % 2 == 1)
-{
-res = res * -1;
-}
-return (res);
+	if (neg % 2 == 1)
+	{
+		res = res * -1;
+	}
+	return (res);
 }
