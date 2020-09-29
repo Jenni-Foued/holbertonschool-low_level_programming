3f625c1e6864934c172b8d1e6f1a1ee50be573b0
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-29 20:05:31 +0100

    fix dat checker

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 /**
  * main - print password.
@@ -6,8 +9,31 @@
  * Return: 0.
  */
 
-int main (void)
+int main(void)
 {
-	printf("~~~~~~~~~~~~~~~~~~~~~~");
+	int ascii = 2772, i = 0, j, random;
+	char password[100];
+	time_t t;
+
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		random = rand() % 126;
+		password[i] = random;
+		ascii -= random;
+		i++;
+	}
+	if (ascii > 0)
+		password[i] = ascii;
+	else
+	{
+		i--;
+	}
+	
+
+	for (j = 0; j <= i; j++)
+	{
+		printf("%c", password[j]);
+	}
 	return (0);
 }
