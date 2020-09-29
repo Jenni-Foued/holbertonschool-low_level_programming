8bfc09787649cba2017ee5fb86dfb32a6e192118
Jenni-Foued <medfouedjenni@gmail.com>
2020-09-29 01:08:35 +0100

    update task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,14 @@
 
 void print_rev(char *s)
 {
-	int i;
-	int length = 0;
+	int i = 0;
+	int length;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (length = 0; s[length] != '\0'; length++)
 	{
-		length++;
 	}
 
-	for (i = length; i >= 0; i--)
+	for (i = length -1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
