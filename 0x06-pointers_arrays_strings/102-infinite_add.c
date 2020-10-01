b7f0f3fa36ba78795052d2142b8d9f4f2d006ea0
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-01 23:27:52 +0100

    this needs a full day by itself

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *infinite_add - add two numbers.
+ *
+ *@n1: first number.
+ *@n2: second number.
+ *@r: result.
+ *@size_r: result size.
+ *Return: the addition of n1 and n2.
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	
+	return (r);
+}
