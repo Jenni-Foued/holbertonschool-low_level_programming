055d9d76bfde0aeb35eb790942463946a716b2ef
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-01 18:54:21 +0100

    task 8 better version

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,30 +8,18 @@
 
 char *rot13(char *s)
 {
-	int i = 0, j;
-	char array_rot13[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-	 'U', 'V'
-	, 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-	 'I',
-	 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-	 'v', 'w',
-	 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-	 'i', 'j', 'k', 'l', 'm'};
-	char array_alpha[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-	 'I', 'J', 'K', 'L',
-	 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-	 'X', 'Y', 'Z', 'a', 'b',
-	 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-	 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-	 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int i = 0, j = 0;
+	char *string_rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm ";
+	char *string_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\0";
 
 	while (s[i] != '\0')
 	{
-		for (j = 0; j < 52; j++)
-		{
-			if (s[i] == array_alpha[j])
-				s[i] = array_rot13[j];
-		}
+		do {
+			if (s[i] == string_alpha[j])
+				s[i] = string_rot13[j];
+			j++;
+		} while (s[i] != string_alpha[j - 1] && string_alpha[j - 1] != '\0');
+		j = 0;
 		i++;
 	}
 
