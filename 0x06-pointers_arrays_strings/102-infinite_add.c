5e9c517419d61243ace78917f9b4b790fd94e3d7
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-01 23:46:46 +0100

    empty files

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -12,6 +12,6 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	
+
 	return (r);
 }
---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *print_buffer - print.
+ *@b: string.
+ *@size: size of char.
+ */
+
+void print_buffer(char *b, int size)
+{
+
+}
