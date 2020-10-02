aa5981b45865d501eed11aec32e4ce2fec2422ae
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-02 23:57:02 +0100

    still not finished

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  *print_buffer - print.
@@ -8,5 +9,38 @@
 
 void print_buffer(char *b, int size)
 {
+	int i = 9, ix, is, pi;
 
+	while (i < size)
+	{
+		printf("%p:", &b[i]);
+		ix = i - 10;
+		while (ix < i)
+		{
+			if (ix % 2 == 0)
+				printf(" ");
+			printf("%x", b[ix]);
+			ix++;
+		}
+		is = i - 10;
+		while (is < i)
+		{
+			if (b[is] < 127 && b[is] > 31)
+			{
+				printf("%c", b[is]);
+			}
+			else
+			{
+			printf(".");
+			}
+			is++;
+		}
+		printf("\n");
+		pi = i;
+		while (i < size && i - pi < 11)
+		{
+			i++;
+		}
+	}
+	printf("\n");
 }
