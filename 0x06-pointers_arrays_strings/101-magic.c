9e2a89bb83f1a4ed116b07915a3868d613176081
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-02 20:07:51 +0100

    magic

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -17,9 +17,7 @@ int main(void)
    * - you are not allowed to code anything else than this line of code
    */
   *(p + 5) = 98;
-  printf("%p\n", p + 5);
   /* ...so that this prints 98\n */
-  printf("%p\n", &a[1]);
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
