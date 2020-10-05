e4568a32cb499bfa42c2ab03452995c55026de96
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-05 15:55:42 +0100

    update task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -19,7 +19,7 @@ void print_diagsums(int *a, int size)
 	printf("%li, ", sum);
 	sum = 0;
 
-	for (i = size - 1; i <= sizem -size +1; i += size - 1)
+	for (i = size - 1; i <= sizem - size + 1; i += size - 1)
 		sum += a[i];
 	printf("%li\n", sum);
 }
