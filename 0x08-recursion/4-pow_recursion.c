5e08b78048f1be2c2f02c75ce3d1ac89c71892c0
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-07 01:42:55 +0100

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_pow_recursion - calculate x power y.
+ *@x: integer.
+ *@y: integer.
+ *Return: int.
+ */
+
+int _pow_recursion(int x, int y)
+
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
