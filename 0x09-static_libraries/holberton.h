01781ffd56525f978a665b3d5885bb47ceed6e68
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-10 17:25:02 +0100

    holberton.h

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,4 +1,6 @@
-#!/bin/bash
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
