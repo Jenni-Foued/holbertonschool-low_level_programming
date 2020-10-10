55f362de9104c50bce1af95ccf88888a82bfb3b3
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-10 17:23:48 +0100

    holberton.h

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,6 +1,4 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
+#!/bin/bash
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +19,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif
