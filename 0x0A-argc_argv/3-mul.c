6b7ac25809724d964b8e6d6cd29cd1da1b458ac2
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-10 14:41:23 +0100

    update task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,12 @@
 
 int main(int argc, char *argv[])
 {
-	int mult = 0;
-
 	if (argc < 3)
 	{
 		printf("ERROR\n");
 		return (1);
 	}
 
-	mult = atoi(argv[1]) * atoi(argv[2]);
-
-	printf("%d\n", mult);
-
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
