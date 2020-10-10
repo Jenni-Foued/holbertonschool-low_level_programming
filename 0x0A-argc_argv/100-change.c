7a8df210bf038adcd8eb2611d2d99a5de0685d90
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-10 15:28:15 +0100

    update 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,40 +10,24 @@
 
 int main(int argc, char *argv[])
 {
-	int cents;
-	int coins = 0;
+	int cents, i, coins = 0;
+	int pieces[] = {25, 10, 5, 2, 1};
 
 	if (argc == 2)
 	{
 		cents = atoi(argv[1]);
-		while (cents > 0)
+		if (cents < 1)
 		{
-			if (cents >= 25)
-			{
-				coins += cents / 25;
-				cents %= 25;
-			}
-			else if (cents >= 10)
-			{
-				coins += cents / 10;
-				cents %= 10;
-			}
-			else if (cents >= 5)
-			{
-				coins += cents / 5;
-				cents %= 5;
-			}
-			else if (cents >= 2)
-			{
-				coins += cents / 2;
-				cents %= 2;
-			}
-			else
-			{
-				coins++;
-				cents = 0;
-			}
+			printf("0\n");
+			return (0);
 		}
+
+		for (i = 0; i < 5 && cents; i++)
+		{
+			coins += cents / pieces[i];
+			cents %= pieces[i];
+		}
+
 		printf("%d\n", coins);
 		return (0);
 	}
