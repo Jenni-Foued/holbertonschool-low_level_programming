8e5ff22b0c8f7b0bedeacab3f7a118631b901402
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-10 15:01:40 +0100

    update task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  *main - print the multiplication of 2 numbers.
@@ -10,16 +11,30 @@
 
 int main(int argc, char *argv[])
 {
-	long int mul;
+	int mul, i;
 
 	if (argc != 3)
 	{
 		printf("ERROR\n");
 		return (1);
 	}
-
 	mul = atoi(argv[1]) * atoi(argv[2]);
-	printf("%ld\n", mul);
-
+	for (i = 0; argv[1][i] ; i++)
+		{
+			if (isdigit(argv[1][i]) == 0)
+			{
+				printf("0\n");
+				return (0);
+			}
+		}
+		for (i = 0; argv[2][i] ; i++)
+		{
+			if (isdigit(argv[2][i]) == 0)
+			{
+				printf("0\n");
+				return (0);
+			}
+		}
+	printf ("%d\n", mul);
 	return (0);
 }
