35e1dbc9bf958a3ff718d248a9743bb1abbd66a5
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-10 15:40:50 +0100

    Revert "update 100"
    This reverts commit 808fda939286a4087a3982e640535a069a34369f.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,25 +13,24 @@ int main(int argc, char *argv[])
 	int cents, i, coins = 0;
 	int pieces[] = {25, 10, 5, 2, 1};
 
-	if (argc != 2)
+	if (argc == 2)
 	{
-		printf("ERROR\n");
-		return (1);
-	}
+		cents = atoi(argv[1]);
+		if (cents < 1)
+		{
+			printf("0\n");
+			return (0);
+		}
 
-	cents = atoi(argv[1]);
-	if (cents < 1)
-	{
-		printf("0\n");
+		for (i = 0; i < 5 && cents; i++)
+		{
+			coins += cents / pieces[i];
+			cents %= pieces[i];
+		}
+
+		printf("%d\n", coins);
 		return (0);
 	}
-
-	for (i = 0; i < 5 && cents; i++)
-	{
-		coins += cents / pieces[i];
-		cents %= pieces[i];
-	}
-
-	printf("%d\n", coins);
-	return (0);
+	printf("ERROR\n");
+	return (1);
 }
