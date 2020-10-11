d6202116a62ddb9e380f82440087b15c42bdbf05
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-11 13:50:07 +0100

    plz

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,24 +12,21 @@ int main(int argc, char *argv[])
 {
 	int sum = 0, i, j;
 
-	if (argc < 2)
+	if (argc > 1)
 	{
-	printf("0\n");
-	return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] ; j++)
+		for (i = 1; i < argc; i++)
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+			for (j = 0; argv[i][j] ; j++)
 			{
-				printf("ERROR\n");
-				return (1);
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("ERROR\n");
+					return (1);
+				}
 			}
-		}
 		sum += atoi(argv[i]);
+		}
 	}
-
 	printf("%d\n", sum);
 	return (0);
 }
