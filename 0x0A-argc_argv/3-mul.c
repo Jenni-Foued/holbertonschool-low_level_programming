8fcc06f71786383f2114e9e76b67d4eaa2da5c8a
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-11 13:29:30 +0100

    checker 5

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
 {
 	int arg1 = 0, arg2 = 0;
 
-	if (argc == 3)
+	if (argc != 3)
 	{
-		arg1 = atoi(argv[1]);
-		arg2 = atoi(argv[2]);
-		printf("%d\n", arg1 * arg2);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	printf("Error\n");
-	return (1);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[2]);
+	printf("%d\n", arg1 * arg2);
+	return (0);
 }
