187f7e557f3a1ba143ab2503c2bd89566bab4f40
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-13 16:05:36 +0100

    fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,12 +8,14 @@
  *Return: pointer to char if success.
  */
 
-char *create_array(unsigned int size, __attribute__((unused))char c)
+char *create_array(unsigned int size, char c)
 {
-	char *tmp;
+	char *tmp = malloc(sizeof(char) * size);
+	unsigned int i;
 
-	if (size == 0)
+	if (size == 0 || tmp == NULL)
 		return (NULL);
-	tmp = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+		tmp[i] = c;
 	return (tmp);
 }
