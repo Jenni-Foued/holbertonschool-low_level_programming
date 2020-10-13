845eba45ac1d90ada8d317d2921eca2ff6098c39
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-13 15:13:43 +0100

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,12 @@
 char *create_array(unsigned int size, __attribute__((unused))char c)
 {
 	char *tmp;
-
+	if (size = 0)
+		return (NULL);
+	
 	tmp = (char *) malloc(sizeof(char) * size);
+	if (tmp = NULL)
+		return (NULL);
+	
 	return (tmp);
 }
