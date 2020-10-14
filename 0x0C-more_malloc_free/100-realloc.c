14b4f6931901eea5b00457b5bb2d60d3456f4024
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-14 19:22:04 +0100

    update task 1 advanced

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,12 +14,23 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 
-
+	if (ptr == NULL)
+	{
+		if (new_size == 0)
+			return (NULL);
+		ptr = malloc(new_size);
+	}
+	else
+	{
 	if (new_size == 0)
 	{
-		if (ptr != NULL)
-			free(ptr);
+		free(ptr);
 		return (NULL);
+	}
+	else if (new_size > old_size)
+	{
+		ptr = malloc(new_size);
+	}
 	}
 	return (ptr);
 }
