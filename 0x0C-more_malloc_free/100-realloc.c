acee387b0dff1ff98fdc388cbac2df3a57dcb39f
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-14 18:37:40 +0100

    testing new size = 0

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocate the size of an array
+ * from old_size to new_size.
+ *
+ * @old_size: old size of ptr.
+ * @new_size: new size of ptr.
+ * @ptr: array.
+ * Return: address of array.
+ **/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	
+	if(new_size == 0)
+	{
+		if (ptr != NULL)
+				free(ptr);
+		return (NULL);	
+	}
+	return(ptr);
+}
