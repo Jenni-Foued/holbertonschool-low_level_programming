ecdf096723cf040cd3aad366be39e61f20a210c8
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-17 23:07:16 +0100

    update

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -2,35 +2,6 @@
 #include <stdlib.h>
 
 /**
- * create_tab - Create an array of strings.
- * @tab: Address of array tab.
- * @pos: Position in string where to start copying.
- * @str: Address of the string to use.
- * Return: Address of array.
- **/
-
-char *create_tab(char *tab, char *str, int pos)
-{
-	int i, lw = 1;
-
-	for (i = pos; str[i] != ' ' && str[i]; i++)
-		lw++;
-
-	tab = malloc(sizeof(char) * lw);
-	if (tab == NULL)
-	{
-		free(tab);
-		return (NULL);
-	}
-
-	for (i = 0; str[pos] != ' ' && str[pos] != '\0'; pos++)
-	{	tab[i] = str[pos];
-		i++;
-	}
-	tab[i] = '\0';
-	return (tab);
-}
-/**
 *count - Count words in a string.
 *@str: String address.
 *Return: Number of words in a string (Integer).
@@ -49,7 +20,6 @@ int count(char *str)
 			words++;
 		i++;
 	}
-
 	return (words);
 }
 
@@ -62,29 +32,41 @@ int count(char *str)
 char **strtow(char *str)
 {
 	char **tab;
-	int words = 0, i, pos = 0;
-/* Check if the string is empty. */
-	if (str == NULL)
-		return (NULL);
-/* Allocate an array */
+	int words, i, j, c = 0, k;
+
 	words = count(str);
-	tab = (char **) malloc(sizeof(char) * words + 1);
+	if (words == 0)
+		return (NULL);
+
+	tab = malloc(sizeof(char) * (words + 1));
 	if (tab == NULL)
 		return (NULL);
 /* Copy the string str's content into the array tab */
-	for (i = 0; i < words; i++)
+	while (str[i])
 	{
-		while (str[pos] == ' ')
-			pos++;
-		tab[i] = create_tab(tab[i], str, pos);
-		if (tab[i] == NULL)
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
-			for (; i >= 0; i--)
-				free(tab[i]);
-			free(tab);
+			for (j = 1; str[i + j] != ' ' && str[i + j]; i++)
+			;
+			tab[c] = malloc((j + 1) * sizeof(char));
+			if (tab[c] == NULL)
+			{
+				for (k = 0; k < c; k++)
+					free(tab[k]);
+				free(tab);
+				return (NULL);
+			}
+			for (k = 0; k < j; k++)
+				tab[c][k] = str[i + k];
+			tab[c][k] = '\0';
+			c++;
+			i += j;
 		}
+		else
+			i++;
 	}
-	tab[i] = malloc(sizeof(char));
+
+	tab[words - 1] = malloc(sizeof(char));
 	tab[i] = NULL;
 	return (tab);
 }
