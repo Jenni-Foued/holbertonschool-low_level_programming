2945b01b4da6be4a94a5da4e747af2c271f65ef8
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-17 22:01:49 +0100

    update

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -2,67 +2,87 @@
 #include <stdlib.h>
 
 /**
-*count - count words in str.
-*@str: string.
-*Return: Number of words in str.
+ * create_tab - Create an array of strings.
+ * @tab: Address of array tab.
+ * @pos: Position in string where to start copying.
+ * @str: Address of the string to use.
+ * Return: Address of array.
+ **/
+
+char *create_tab(char *tab, char *str, int pos)
+{
+	int i, lw = 1;
+
+	for (i = pos; str[i] != ' ' && str[i]; i++)
+		lw++;
+
+	tab = malloc(sizeof(char) * lw);
+	if (tab == NULL)
+	{
+		free(tab);
+		return (NULL);
+	}
+
+	for (i = 0; str[pos] != ' ' && str[pos] != '\0'; pos++)
+	{	tab[i] = str[pos];
+		i++;
+	}
+	tab[i] = '\0';
+	return (tab);
+}
+/**
+*count - Count words in a string.
+*@str: String address.
+*Return: Number of words in a string (Integer).
 */
 
 int count(char *str)
 {
-int i = 0, words = 0;
+	int i = 0, words = 0;
 
 	while (str[i] == ' ')
-	{
 		i++;
-	}
+
 	while (str[i] != '\0')
 	{
-		if (str[i] != ' ' && (str[i + 1] == '\0'|| str[i + 1] == ' '))
+		if (str[i] != ' ' && (str[i + 1] == '\0' || str[i + 1] == ' '))
 			words++;
 		i++;
 	}
+
 	return (words);
 }
 
 /**
- *strtow - copy each word of a string into an array of strings(words).
- *@str: pointer to string.
- *Return: pointer to array of strings.
+ *strtow - Copy each word of a string into an array of strings(words).
+ *@str: String address.
+ *Return: Array of strings address.
  */
 
 char **strtow(char *str)
-{	char **tab;
-	int words = 0, i = 0, lw, j = 0, k = 0, n;
-
+{
+	char **tab;
+	int words = 0, i, pos = 0;
+/* Check if the string is empty. */
 	if (str == NULL)
 		return (NULL);
+/* Allocate an array */
 	words = count(str);
 	tab = (char **) malloc(sizeof(char) * words + 1);
 	if (tab == NULL)
 		return (NULL);
-	for (; i < words; i++)
-	{	lw = 0;
-		n = 0;
-		for (; str[j] == ' ' ; j++)
-		;
-		for (; str[j] != ' '; j++)
-			lw++;
-		lw++;
-		tab[i] = (char *) malloc(sizeof(char) * lw);
+/* Copy the string str's content into the array tab */
+	for (i = 0; i < words; i++)
+	{
+		while (str[pos] == ' ')
+			pos++;
+		tab[i] = create_tab(tab[i], str, pos);
 		if (tab[i] == NULL)
 		{
 			for (; i >= 0; i--)
 				free(tab[i]);
 			free(tab);
-			return (NULL);
 		}
-		for (; str[k] == ' ' ; k++)
-		;
-		for (; str[k] != ' '; k++)
-		{	tab[i][n] = str[k];
-			n++;
-		}
-		tab[i][n] = '\0';
 	}
 	tab[i] = malloc(sizeof(char));
 	tab[i] = NULL;
