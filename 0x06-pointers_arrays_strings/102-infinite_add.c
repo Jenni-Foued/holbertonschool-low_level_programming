8541ff92957d85aa5b607f4e8e11030371f53b59
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-18 20:39:11 +0100

    fixed(changed the solution)

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,18 +1,5 @@
 #include "holberton.h"
-/**
- * length - Calculate the length of a string.
- * @n: String address.
- * Return: integer.
- **/
-int length(char *n)
-{
-	int i = 0;
 
-	while (n[i])
-		i++;
-	i--;
-	return (i);
-}
 /**
  *infinite_add - add two numbers.
  *
@@ -25,29 +12,36 @@ int length(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int add = 0, len1, len2, s;
+	int add = 0, len1, len2, i, j;
 
-	len1 = length(n1);
-	len2 = length(n2);
-	s = size_r - 1;
-	r[s] = '\0';
-	s--;
-	for (; s >= 0; s--, len1--, len2--)
+	for (len1 = 0; n1[len1]; len1++)
+	;
+	for (len2 = 0; n2[len2]; len2++)
+	;
+	if (len1 > size_r || len2 > size_r)
+		return (0);
+	len1--;
+	len2--;
+	size_r--;
+	for (i = 0; i < size_r; i++, len1--, len2--)
 	{
 		if (len1 >= 0)
-		{
 			add += n1[len1] - '0';
-		}
 		if (len2 >= 0)
-		{
 			add += n2[len2] - '0';
-		}
-		if ((len1 < 0 && len2 < 0) && add == 0)
+		if (len1 < 0 && len2 < 0 && add == 0)
 			break;
-		r[s] = (add % 10) + '0';
+		r[i] = add % 10 + '0';
 		add /= 10;
 	}
+	r[i] = '\0';
 	if (len1 >= 0 || len2 >= 0 || add)
 		return (0);
+	for (i--, j = 0; i > j; i--, j++)
+	{
+		add = r[i];
+		r[i] = r[j];
+		r[j] = add;
+	}
 	return (r);
 }
