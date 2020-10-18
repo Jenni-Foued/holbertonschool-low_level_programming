33c4c2be58bf7b728de369a9656ace6b6f9f625c
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-18 01:19:32 +0100

    not done yet

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,5 +1,18 @@
 #include "holberton.h"
+/**
+ * length - Calculate the length of a string.
+ * @n: String address.
+ * Return: integer.
+ **/
+int length(char *n)
+{
+	int i = 0;
 
+	while (n[i])
+		i++;
+	i--;
+	return (i);
+}
 /**
  *infinite_add - add two numbers.
  *
@@ -12,6 +25,30 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int add = 0, len1, len2, s;
 
+	len1 = length(n1);
+	len2 = length(n2);
+	s = size_r - 1;
+	r[s] = '\0';
+	s--;
+	while (s >= 0 && (len1 >= 0 || len2 >= 0 || add != 0))
+	{
+		if (len1 >= 0)
+		{
+			add += n1[len1] - '0';
+		}
+		if (len2 >= 0)
+		{
+			add += n2[len2] - '0';
+		}
+		r[s] = (add % 10) + '0';
+		add /= 10;
+		s--;
+		len1--;
+		len2--;
+	}
+	if (len1 >= 0 || len2 >= 0 || add)
+		return (0);
 	return (r);
 }
