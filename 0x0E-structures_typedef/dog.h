c63b27f0915f143fe2a2d76edf96fcdceee64c40
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-19 14:38:42 +0100

    task 0 header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog description.
+ * @name: Dog name.
+ * @age: Dog age.
+ * @owner: Dog's owner.
+ **/
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
