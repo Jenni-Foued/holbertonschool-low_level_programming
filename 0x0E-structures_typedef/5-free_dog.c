58ceeab7962fa492c0280e4fcfb77e140e799d3c
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-19 16:52:02 +0100

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Free the structure dog.
+ * @d: Address of d.
+ **/
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 int _putchar(char c);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
