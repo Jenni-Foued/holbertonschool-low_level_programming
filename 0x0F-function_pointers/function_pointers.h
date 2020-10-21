29ba94448321565d595d55cc9b6cf53f4a836a64
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-21 21:04:07 +0100

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
 #ifndef FN_H
 #define FN_H
+#include <stddef.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, int size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
