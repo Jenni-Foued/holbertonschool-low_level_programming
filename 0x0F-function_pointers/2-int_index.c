d9f3883d9ade93adb9b39b241b2987277f40e10a
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-21 21:15:00 +0100

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,12 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
+	if (array && cmp)
+	{	
+		if (size <= 0)
+			return (-1);
+		for (; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 		return (-1);
-	for (; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
-			return (i);
 	}
-	return (-1);
 }
