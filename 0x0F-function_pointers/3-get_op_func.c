977c50531a5b0367840b1f00d28bd09badebb0e1
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-21 21:50:12 +0100

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * get_op_func - Find the equivalent function.
  * @s: Operand.
@@ -21,9 +21,9 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (s == ops[i].op)
-			return (ops[i].f);
+		if (strcmp(s, ops[i].op) == 0)
+			break;
 		i++;
 	}
-	return (NULL);
+return (ops[i].f);
 }
