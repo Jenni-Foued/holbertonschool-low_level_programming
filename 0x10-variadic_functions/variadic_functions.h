d9cb0e103aac0fd52426dc5baf88580e17f5c2ff
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-25 17:53:59 +0100

    header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VARDIAC_H
+#define VARDIAC_H
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+typedef struct format
+{
+char form;
+void (*f)(va_list);
+} format_t;
+void print_all(const char * const format, ...);
+
+#endif
