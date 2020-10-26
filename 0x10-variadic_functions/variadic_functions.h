3c1f914aa459c149a341044888d112ee95a15d05
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-26 16:09:42 +0100

    add description to struct

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,12 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct format - Types.
+ * @form: Type symboles.
+ * @f: Function pointer to the corresponding type function.
+ **/
 typedef struct format
 {
 char form;
