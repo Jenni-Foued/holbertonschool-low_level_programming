91edceeebb7c61b14853c8bce81907a146f35ded
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-30 19:09:58 +0100

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,10 +2,14 @@ section .data
     text db "Hello, Holberton",10,0
 section .text
     global main
- 
+
 main:
     mov rax, 1
     mov rdi, 1
     mov rsi, text
     mov rdx, 17
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
     syscall
