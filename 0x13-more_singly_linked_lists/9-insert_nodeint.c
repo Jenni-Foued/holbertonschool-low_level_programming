86a33ef63dae34aa92d439d8ffce8c2716f0e1e5
Jenni-Foued <medfouedjenni@gmail.com>
2020-11-04 12:37:53 +0100

    fixed betty style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	if (idx != 0)
 		temp = *head;
-	for (;i < idx - 1 && temp != NULL; i++)
+	for (; i < idx - 1 && temp != NULL; i++)
 		temp = temp->next;
 	if (temp == NULL)
 		return (NULL);
