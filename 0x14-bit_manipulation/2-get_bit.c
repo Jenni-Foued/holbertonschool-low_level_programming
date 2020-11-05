c099eb8a5d4c782cbfd20b2caf619d5afaf1a634
Jenni-Foued <medfouedjenni@gmail.com>
2020-11-05 20:32:48 +0100

    gotcha intial

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * get_bit - Target the bit of a number at the index position.
+ * @n: Given number.
+ * @index: Given index.
+ * Return: Positive number if success (0,1) or -1 if it fails.
+ **/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i = 0;
+
+	for (; i < index; i++)
+	{
+		n >>=  1;
+		if (n == 0)
+			return (-1);
+	}
+
+	n %= 2;
+	return (n);
+}
