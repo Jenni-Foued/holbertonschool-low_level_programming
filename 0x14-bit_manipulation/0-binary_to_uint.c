6d31df3d20cf109e55db2acc9c963b4e7a7b6f6b
Jenni-Foued <medfouedjenni@gmail.com>
2020-11-05 08:21:46 +0100

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Address of a string that contains binary code.
+ * Return: unsigned int.
+ **/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int multiplier = 1, res = 0;
+
+	if (b[i] == '\0')
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * multiplier);
+		multiplier *= 2;
+		i--;
+	}
+	return (res);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif
