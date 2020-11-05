c39f91117f2e12d8163d50de6e7cf75a5616c75a
Jenni-Foued <medfouedjenni@gmail.com>
2020-11-05 19:54:23 +0100

    rebuild

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,11 @@
 #include "holberton.h"
 
-/**
- * print_binary - Print binary code of an unsigned long int (n).
- * @n: Number to print in binary.
- **/
-
 void print_binary(unsigned long int n)
 {
-	int i = 0;
-	char res[33];
 
-	if (n == 0)
-		_putchar('0');
-
-	for (; n > 0; i++)
+	if (n > 1)
 	{
-		if ((n & 1) == 1)
-			res[i] = '1';
-		else
-			res[i] = '0';
-		n >>= 1;
+		print_binary (n >> 1);
 	}
-	res[i] = '\0';
-	i--;
-	for (; i >= 0; i--)
-		_putchar(res[i]);
+	_putchar ((n & 1) + '0');
 }
