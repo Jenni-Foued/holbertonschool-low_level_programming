c992dc389c372a80b5754ea72112ade2c924e27a
Jenni-Foued <medfouedjenni@gmail.com>
2020-11-05 21:14:49 +0100

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,13 +11,13 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
-	for (; i < index; i++)
+	for (; i < index, (n != 1 || n != 0) ; i++)
 	{
 		n >>=  1;
-		if (n == 0)
+		if (n == 0 && i)
 			return (-1);
 	}
-
-	n %= 2;
+	if (i == index)
+		n %= 2;
 	return (n);
 }
