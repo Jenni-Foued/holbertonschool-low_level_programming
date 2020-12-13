e07074f6ebd20ea3a381f7424aab8510cfad8c50
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-13 12:39:17 +0100

    Task 6: removed trailing whitespace

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) 
+ * sum_dlistint - returns the sum of all the data (n)
  * of a dlistint_t linked list.
  *
  * @head: pointer to the first node of the list.
  *
- * Return: sum of all the data or 0 if list is empty.
+ * Return: sum of all the data (n) or 0 if list is empty.
  */
 int sum_dlistint(dlistint_t *head)
 {
