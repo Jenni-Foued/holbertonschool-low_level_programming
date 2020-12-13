60c9264479bf955af82c195b53093c377d1fe1bb
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-13 12:36:19 +0100

    Task 6 & update header file

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data (n) 
+ * of a dlistint_t linked list.
+ *
+ * @head: pointer to the first node of the list.
+ *
+ * Return: sum of all the data or 0 if list is empty.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
