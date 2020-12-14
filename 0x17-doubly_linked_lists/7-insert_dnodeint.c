e264f8f765ce8e5adbe93387219695be5a1926b2
Jenni-Foued <medfouedjenni@gmail.com>
2020-12-14 12:44:01 +0100

    Task 7: malloc check

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,6 +33,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = n;
 	new_node->prev = temp;
 	new_node->next = temp->next;
