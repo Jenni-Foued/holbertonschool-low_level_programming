3013f5cb6f7bd1c69fb6a943cf1d8f893733a54b
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-20 15:07:32 +0100

    Check array is not null

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -15,24 +15,26 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t pos, low = 0, high = size - 1;
 
-	while ((array[high] != array[low]) && (value >= array[low]))
+	if (size > 0 && array)
 	{
-		pos = low + (((double)(high - low) / (array[high] - array[low]))
-		* (value - array[low]));
-		if (pos > high)
+		while ((array[high] != array[low]) && (value >= array[low]))
 		{
-			printf("Value checked array[%ld] is out of range\n", pos);
-			return (-1);
+			pos = low + (((double)(high - low) / (array[high] - array[low]))
+			* (value - array[low]));
+			if (pos > high)
+			{
+				printf("Value checked array[%ld] is out of range\n", pos);
+				return (-1);
+			}
+			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+			if (array[pos] < value)
+				low = pos + 1;
+			else if (value < array[pos])
+				high = pos - 1;
+			else
+				return (pos);
 		}
-		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
-		if (array[pos] < value)
-			low = pos + 1;
-		else if (value < array[pos])
-			high = pos - 1;
-		else
-			return (pos);
 	}
-
 	if (value == array[low])
 		return (low);
 	else
